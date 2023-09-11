e7bd12fbccac0f7894477158e8e4c09721cbb9f8
Ryan Iug <ryaniugramos@gmail.com>
2023-09-11 09:14:35 -0300

    moment search tool

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { MomentService } from 'src/app/services/moment.service';
 import { Component, OnInit } from '@angular/core';
 import { IMoment } from 'src/app/IMoment';
 import { environment } from 'src/environments/environment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +13,9 @@ export class HomeComponent implements OnInit {
   apiUrl = environment.baseApiUrl;
   allMoments: IMoment[] = [];
   searchResults: IMoment[] = [];
+
+  faSearch = faSearch;
+  searchTerm: string = '';
 
   constructor(private momentService: MomentService) {}
 
@@ -26,5 +30,14 @@ export class HomeComponent implements OnInit {
       this.allMoments = data;
       this.searchResults = data;
     });
+  }
+
+  search(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+
+    this.searchResults = this.allMoments.filter((moment) =>
+      moment.title?.toLowerCase().includes(value)
+    );
   }
 }
